8a4b6b3a4a9e093bd5f62b4c96611dfb60cad9cf
Camelia Mosor <crm782@nau.edu>
2025-11-17 19:46:05 -0700

    Fixed errors

--- db/db.js
+++ db/db.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const axios = require('axios');
 
 // The Pool will automatically use the environment variables
 const pool = new Pool({
@@ -9,22 +10,24 @@ const pool = new Pool({
   port: 5432,
 });
 
-(async () => {
-try {
+async function createDB() {
+  try {
   const response = await axios.get("https://openlibrary.org/subjects/love.json?limit=20");
   const books = response.data.works;
   books.forEach((book) => {
-    const id = (book.key).substring(6);
-    (async () => {
-      await database.query("INSERT INTO books (title, author, date, coverSmall, coverLarge) VALUES ($1, $2, $3, $4, $5)", [book.title, book.authors[0].name, book.first_publish_year, "https://covers.openlibrary.org/w/olid" + id + "-M.jpg", "https://covers.openlibrary.org/w/olid" + id + "-L.jpg"]);
-    });
-    });
+    addBook(book);
+  });
 }
 catch (error) {
   console.error("Failed to make request:", error.message);
-}
-}
-);
+}};
+
+async function addBook(book) {
+  const id = (book.key).substring(6);
+  await pool.query("INSERT INTO books (title, author, date, coverSmall, coverLarge) VALUES ($1, $2, $3, $4, $5)", [book.title, book.authors[0].name, book.first_publish_year, "https://covers.openlibrary.org/w/olid" + id + "-M.jpg", "https://covers.openlibrary.org/w/olid" + id + "-L.jpg"]);
+};
+
+createDB();
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
